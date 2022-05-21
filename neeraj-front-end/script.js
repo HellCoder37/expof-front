1edf7ac7853265331310a1059f34215ca3672c7f
Neeraj Rastogi <98451723+HellCoder37@users.noreply.github.com>
2022-05-21 10:14:06 +0530

    stuff

---
+++ neeraj-front-end/script.js
@@ -0,0 +1,12 @@
+let p2 = document.getElementsByClassName("p2")
+let p3 = document.getElementsByTagName("p")
+function change() {
+    let p1 = document.getElementById("p1");
+    p1.innerHTML = "i am  paragraph 1";
+
+
+}
+function c() {
+    let p3 = document.getElementsByTagName("p")
+    p3[1].innerHTML = "i am paragraph";
+}
